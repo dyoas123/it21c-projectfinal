eb94792c9ac0b141a407bedc5b66e4ce5e495255
dyoas123 <rautrautjoash1@gmail.com>
2023-11-17 14:50:31 +0800

    added my first package

---
+++ src/dyoaspackage/Pakeds.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dyoaspackage;
+
+/**
+ *
+ * @author admin
+ */
+public class Pakeds {
+    
+}
